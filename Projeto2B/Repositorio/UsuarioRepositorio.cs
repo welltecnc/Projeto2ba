c0c73d373e1a6d2677fff78d8922c194a85cae88
Wellington <welltecnic@hotmail.com>
2025-03-21 22:10:21 -0300

    criando a os metodos adicionar usuario e obter usuario

--- Projeto2B/Repositorio/UsuarioRepositorio.cs
+++ Projeto2B/Repositorio/UsuarioRepositorio.cs
@@ -1,6 +1,61 @@
-﻿namespace Projeto2B.Repositorio
+﻿using Projeto2B.Models;
+
+namespace Projeto2B.Repositorio
 {
     public class UsuarioRepositorio
     {
+        //PREPARANDO A ESTRUTURA DA CONEXÃO COM O BANCO DE DADOS
+
+        private readonly string _connectionString;
+
+        //CONSTRUTOR
+        public UsuarioRepositorio(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        // CRIANDO O METODO ADICIONAR USUARIO
+
+        public void AdicionarUsuario(Usuario usuario)
+        {
+            using (var db = new Conexao(_connectionString))
+            {
+                var cmd = db.MySqlCommand();
+                cmd.CommandText = "INSERT INTO Usuario (Nome, Email, Senha) VALUES (@Nome,@Email,@Senha)";
+                cmd.Parameters.AddWithValue("@Nome", usuario.Nome);
+                cmd.Parameters.AddWithValue("@Email", usuario.Email);
+                cmd.Parameters.AddWithValue("@Senha", usuario.Senha);
+                cmd.ExecuteNonQuery();
+
+            }
+        }
+        public Usuario ObterUsuario(string email)
+        {
+            using (var db = new Conexao(_connectionString))
+            {
+                var cmd = db.MySqlCommand();
+                cmd.CommandText = "SELECT * FROM Usuario WHERE Email = @Email";
+                cmd.Parameters.AddWithValue("@Email", email);
+                cmd.ExecuteNonQuery();
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return new Usuario
+                        {
+                            Id = reader.GetInt32("Id"),
+                            Nome = reader.GetString("Nome"),
+                            Email = reader.GetString("Email"),
+                            Senha = reader.GetString("Senha"),
+
+                        };
+
+                    }
+
+                }
+                return null;
+            }
+        }
     }
 }
