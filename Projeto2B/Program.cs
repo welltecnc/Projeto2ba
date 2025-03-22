9652c58a61eab8c8055164652d0aad2132123fba
Wellington <welltecnic@hotmail.com>
2025-03-21 22:48:48 -0300

    ajustes

--- Projeto2B/Program.cs
+++ Projeto2B/Program.cs
@@ -1,7 +1,15 @@
+using Projeto2B.Repositorio;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//ADICIONADO O USUARIO REPOSITORIO COMO SERVIÇO
+
+builder.Services.AddScoped<UsuarioRepositorio>();
+
+
 
 var app = builder.Build();
 
