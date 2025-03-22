1e6f2977994f29da57449f96d29ffe5cc4a97bb9
Wellington <welltecnic@hotmail.com>
2025-03-21 21:10:10 -0300

    Criando requisição login,cadastro e model usuario

--- Projeto2B/Controllers/LoginController.cs
+++ Projeto2B/Controllers/LoginController.cs
@@ -1,10 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Projeto2B.Repositorio;
 
 namespace Projeto2B.Controllers
 {
     public class LoginController : Controller
     {
+        //DECLARA UMA VARIAVEL PRIVADA SOMENTE LEITURA 
+        private readonly UsuarioRepositorio _usuarioRepositorio;
+
+        //CONSTRUTOR
+
+        public LoginController (UsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+
+        }
+
         public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string email,string senha)
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro()
         {
             return View();
         }
